caf42edf45f238e73cff361ac70da30f842f6dee
MADELINE PETERS <madelinepet@hotmail.com>
2018-08-17 15:11:24 -0700

    Data structure complete

---
+++ data_structures/linked_list/node.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+
+class Node:
+    def __init__(self, val: Any, _next=None):
+        """ Initializes the class with value, and a next that can be a
+        """
+        self.val = val
+        self._next = _next
+
+    def __str__(self):
+        """ Returns a string
+        """
+        return f'{self.val}'
+
+    def __repr__(self):
+        """ Returns a more highly formatted string
+        """
+        return f' <Node | Val: {self.val | Next: self._next}>'
