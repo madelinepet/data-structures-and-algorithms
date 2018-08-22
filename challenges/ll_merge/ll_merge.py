94ed7971cfbb2d8f4373fbafbf410d160dab096f
MADELINE PETERS <madelinepet@hotmail.com>
2018-08-22 15:32:54 -0700

    Challenge complete and tests written

---
+++ challenges/ll_merge/ll_merge.py
@@ -0,0 +1,118 @@
+import pytest
+
+from .node import Node
+from typing import Any
+
+
+class LinkedList(object):
+    """ This creates a linked list class with methods below
+    """
+    def __init__(self, potential_iterable=None):
+        """ Initializes fn, defines datatype as always a node, = "type annotation"
+        """
+        self.head: Node = None
+        self._length: int = 0
+        if potential_iterable is not None:
+            try:
+                for i in potential_iterable:
+                    self.insert(i)
+            except TypeError:
+                self.insert(potential_iterable)
+
+    def __str__(self):
+        """ Returns a string of the head and the length
+        """
+        return f'{self.head} | Length: {self._length}'
+
+    def __repr__(self):
+        """ Returns a formatted string of the head and the length of the linked list
+        """
+        return f'<Linked List | Head: {self.head} | Length : {self._length}>'
+
+    def __len__(self):
+        """ Returns the length of the ll
+        """
+        return self._length
+
+    def __iter__(self):
+        """ Iterates over the ll
+        """
+        pass
+
+    def __next__(self):
+        """ Iterates to the next item.
+        """
+        pass
+
+    def insert(self, val: Any) -> Any:
+        """ Insert something into the linked list. Any data type can be inserted (denoted by val: datatype), returns any datatype as output
+        """
+        new_node = Node(val, self.head)
+        self.head = new_node
+        self._length += 1
+        return
+
+    def includes(self, val: Any) -> bool:
+        """ Checks to see if something in the list
+        """
+        current_node = self.head
+        while current_node is not None:
+            if current_node.val == val:
+                return True
+            current_node = current_node._next
+        return False
+
+    def append(self, val):
+        """
+        Similar to insert, but appends to end of LL
+        """
+        current = self.head
+        while current._next is not None:
+            current = current._next
+
+        new_node = Node(val, _next=None)
+        current._next = new_node
+
+    def insert_before(self, val, target):
+        """
+        Inserts before a given value
+        """
+        if self.head.val == target:
+            self.head = Node(val, self.head)
+            return self.head
+        else:
+            current = self.head
+            while current._next.val != target:
+                current = current._next
+            current._next = Node(val, current._next)
+            return current._next
+
+    def insert_after(self, val, target):
+        """
+        Inserts after a given value
+        """
+        current = self.head
+        while current.val != target:
+            current = current._next
+        current._next = Node(val, current)
+        return current._next
+
+    def ll_merge(self, ll_two):
+        try:
+            ll_one_current = self.head
+            ll_two_current = ll_two.head
+
+            while ll_one_current is not None and ll_two_current is not None:
+                ll_one_next = ll_one_current._next
+                ll_two_next = ll_two_current._next
+
+                ll_one_current._next, ll_two_current._next = ll_two_next, ll_one_next
+                ll_one_current = ll_one_next
+            ll_two.head = ll_two_current
+
+        except None:
+            'Not valid imput'
+
+
+
+
---
+++ challenges/ll_merge/test_ll_merge.py
@@ -0,0 +1,134 @@
+from .ll_merge import LinkedList
+import pytest
+
+
+@pytest.fixture
+def empty_list():
+    return LinkedList()
+
+
+@pytest.fixture
+def empty_list_two():
+    return LinkedList()
+
+
+@pytest.fixture
+def small_list():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    ll.insert(4)
+    return ll
+
+
+@pytest.fixture
+def small_list_two():
+    ll = LinkedList()
+    ll.insert(a)
+    ll.insert(b)
+    ll.insert(c)
+    ll.insert(d)
+    return ll
+
+
+def test_linked_list_exists():
+    assert small_list
+
+
+def test_create_instance_of_ll():
+    ll = LinkedList()
+    assert isinstance(ll, LinkedList)
+
+
+def test_default_property_head(empty_list):
+    assert empty_list.head is None
+
+
+def test_default_property_length(empty_list):
+    assert empty_list._length == 0
+
+
+def test_insert_successful(empty_list):
+    assert empty_list.head is None
+    empty_list.insert(25)
+    assert empty_list.head.val == 25
+
+
+def test_length_of_list_increases_on_insertion(empty_list):
+    assert len(empty_list) == 0
+    empty_list.insert(25)
+    assert len(empty_list) == 1
+
+
+def test_includes_returns_true_if_exists(small_list):
+    actual = small_list.includes(4)
+    assert actual is True
+    assert small_list.includes(1) is True
+
+
+def test_includes_returns_false_if_not_exists(small_list):
+    assert small_list.includes(100) is False
+    assert small_list.includes(0) is False
+
+
+def test_insertion_of_iterable():
+    ll = LinkedList([1, 2, 3])
+    assert len(ll) == 3
+
+
+def test_create_instance_non_iterable():
+    ll = LinkedList(1)
+    assert len(ll) == 1
+
+
+def test_linked_list_append_exists():
+    assert LinkedList.append
+
+
+def test_append_after_places_new_node_at_end(small_list):
+    """ Test if we added the value to the end of the list
+    """
+    a = 42
+    expected = len(small_list)
+    small_list.append(a)
+    actual = len(small_list)
+    assert small_list.includes(a)
+    assert expected == actual
+
+
+def test_linked_list_append_insert_before():
+    """ tests fn insert before exists
+    """
+    assert LinkedList.insert_before
+
+
+def test_linked_list_append_insert_after():
+    """ test insert_after exists
+    """
+    assert LinkedList.insert_after
+
+
+def test_linked_list_merge_returns_not_valid_list(empty_list, empty_list_two):
+    """ assert empty lists give back None
+    """
+    expected = None
+    actual = LinkedList.ll_merge(empty_list, empty_list_two)
+    assert expected == actual
+
+
+def test_linked_list_merge_merges_lists_equal_lengths(small_list, empty_list):
+    """ Test empty list and list input gives back list
+    """
+    expected = small_list
+    actual = LinkedList.ll_merge(small_list, empty_list)
+    assert expected == actual
+
+
+def test_linked_list_ll_merge_exists():
+    """ Test ll_merge exists
+    """
+    assert LinkedList.ll_merge
+
+
+
