f0b344db089ed4b6861f521c0c67d3540cf9f890
MADELINE PETERS <madelinepet@hotmail.com>
2018-08-26 17:49:16 -0700

    Queue complete, but issue with enqueue.

---
+++ data_structures/queue/README.md
@@ -0,0 +1,13 @@
+# Queue
+Class Queue takes in an iterable. Len, enqueue, and decueue methods defined
+
+## Getting started
+Clone the repository, setup your virtual environment using "pipenv shell", "pipenv install" will download pytest. Run "pytest"
+
+## Solo-assignment
+Madeline Peters
+
+## Resources
+https://www.pythoncentral.io/use-queue-beginners-guide/ 
+
+
---
+++ data_structures/queue/queue.py
@@ -0,0 +1,58 @@
+import pytest
+
+from .node import Node
+
+
+class Queue(object):
+    """ This creates a queue class with methods below
+    """
+    def __init__(self, potential_iterable=None):
+        """ Initializes fn, defines datatype as always a node, = "type annotation"
+        """
+        self.queue = list()
+        self.front = None
+        self.back = None
+        self._length: int = 0
+
+    def __str__(self):
+            """ Returns a string of the top and the length
+            """
+            return f'{self.front} | {self.back}| Length: {self._length}'
+
+    def __repr__(self):
+        """ Returns a formatted string of the top and the length of the queue
+        """
+        return f'<Queue | front: {self.front} | back: {self.back}| Length : {self._length}>'
+
+    def __len__(self):
+        """ Returns the length of the queue
+        """
+        return self._length
+
+    def enqueue(self, potential_iterable):
+        """Takes in an iterable and creates new nodes in the queue's end
+        """
+        if potential_iterable is iter:
+            try:
+                for i in potential_iterable:
+                    if i not in self.queue:
+                        self.queue.insert(0, i)
+                        return True
+                    else:
+                        return False
+            except TypeError:
+                self.insert(0, potential_iterable)
+        else:
+                self.queue.insert(0, potential_iterable)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            return self.queue.pop()
+        return('No items in queue!')
+
+
+
+
+
+
+
