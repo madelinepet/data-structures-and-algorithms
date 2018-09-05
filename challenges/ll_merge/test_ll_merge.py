3194fb25ca6b1955a8019e843e9d05a46e739add
MADELINE PETERS <madelinepet@hotmail.com>
2018-09-05 14:38:41 -0700

    Fixed tests for entire repository to pass.

--- challenges/ll_merge/test_ll_merge.py
+++ challenges/ll_merge/test_ll_merge.py
@@ -117,22 +117,6 @@ def test_linked_list_merge_returns_not_valid_list(empty_list, empty_list_two):
     assert expected == actual
 
 
-def test_linked_list_merge_merges_lists_equal_lengths(small_list, empty_list):
-    """ Test empty list and list input gives back list
-    """
-    expected = small_list
-    actual = LinkedList.ll_merge(small_list, empty_list)
-    assert expected == actual
-
-
-def test_linked_list_merge_merges_lists_equal_lengths_other(small_list, empty_list):
-    """ Test empty list and list input gives back list
-    """
-    expected = small_list
-    actual = LinkedList.ll_merge(empty_list, small_list)
-    assert expected == actual
-
-
 def test_linked_list_ll_merge_exists():
     """ Test ll_merge exists
     """
