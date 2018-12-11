5601de51c6b90c7b5fef02c6cbccd482f0849434
MADELINE PETERS <madelinepet@hotmail.com>
2018-12-10 16:16:25 -0800

    ll fixed methods of insert before and insert after and manually tested them. Next I will write tests.

--- javascript/linkedlist.js
+++ javascript/linkedlist.js
@@ -1,8 +1,8 @@
 class Node {
-    constructor(val)
+    constructor(val, next = null)
     {
         this.val = val;
-        this.next = null;
+        this.next = next;
     }
 }
 
@@ -13,10 +13,56 @@ class LinkedList {
         this.head = null;
         this.length = 0;
     }
+
     insert(val) {
         var newNode = new Node(val);
         this.head = newNode;
         this.length += 1;
         return val;
+    }
+
+    includes(val) {
+        var currentNode = this.head;
+        while (currentNode) {
+            if(currentNode.val === val) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    append(val) {
+        var current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        var newNode = new Node(val);
+        current.next = newNode;
+    }
+
+    insertBefore(val, target) {
+        if (this.head.val === target) {
+            var current = this.head;
+            this.head = new Node(val, this.head);
+            this.head.next = current;
+            return this.head;
+        }
+        else {
+            current = this.head;
+            while(current.next.val !== target) {
+                current = current.next;
+            }
+            current.next = new Node(val, current.next);
+            return current.next;
+        }
+    }
+    insertAfter(val, target) {
+        var current = this.head;
+        while (current.val !== target) {
+            current = current.next;
+        }
+        current.next = new Node(val, current.next);
+        return current.next;
     }
 }
